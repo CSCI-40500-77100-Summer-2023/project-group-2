f38282ee8b11fada36f9244cdc9b5bb3d3d85a41
ReginaldOfori <97319184+ReginaldOfori@users.noreply.github.com>
2023-07-03 22:27:57 -0400

    Comment on task.jsx code

--- src/assets/components/home/Task.jsx
+++ src/assets/components/home/Task.jsx
@@ -13,3 +13,16 @@ const Task = ({ taskName, taskDescription, note, deadline, remindMe }) => {
   };
 
   export default Task;
+
+/* functional component called "Task" using arrow function syntax in JavaScript.
+  The component takes in several props as parameters: taskName, taskDescription, 
+  note, deadline, and remindMe.
+
+Inside the component, it initializes a variable called "reminder" with the value 'Yes'. 
+It then checks if the value of the prop remindMe is not equal to 'on'. 
+If the condition is true, it updates the value of the "reminder" variable to 'No'.
+
+The component returns a JSX (JavaScript XML) template that renders the task details. 
+It includes an h1 element displaying the taskName, and several p elements displaying the taskDescription, 
+note, deadline, and the value of the "reminder" variable (which indicates whether there is a reminder or not).
+*/
