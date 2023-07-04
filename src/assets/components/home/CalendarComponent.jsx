db9a4db1210cc39ce20249ed1d3d4503711cc81d
Christopher Mena <mena.christopher.student@gmail.com>
2023-07-03 23:02:42 -0400

    working notification

--- src/assets/components/home/CalendarComponent.jsx
+++ src/assets/components/home/CalendarComponent.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import Task from './Task';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const CalendarComponent = () => {
     const [value, onChange] = useState(new Date().toISOString().substring(0, 10));
     const [selectedTask, setSelectedTask] = useState(null);
@@ -13,20 +16,79 @@ const CalendarComponent = () => {
         const formData = new FormData(e.target);
         // Create object from form data
         const data = Object.fromEntries(formData);
+
+        // Validate input fields
+        if (!data.taskName) {
+            createNotification("warning", "Task Name is required");
+            return;
+        }
+        if (!data.taskDescription) {
+            createNotification("warning", "Task Description is required");
+            return;
+        }
+        if (!data.note) {
+            createNotification("warning", "Note is required");
+            return;
+        }
+        if (!data.deadline) {
+            createNotification("warning", "Deadline is required");
+            return;
+        }
+
         // Add object to info array
-        if(info.has(data.deadline)) {
+        if (info.has(data.deadline)) {
             // Add to existing array
             const temp = info.get(data.deadline);
             temp.push(data);
             info.set(data.deadline, temp);
-        }
-        else {
+        } else {
             // Create new array
             info.set(data.deadline, [data]);
         }
+
         // Reset form
         e.target.reset();
+        createNotification("success", "Form submitted successfully!");
+        checkDeadline(new Date(data.deadline));
     }
+
+    // Check if date is close to deadline, then create notification
+    // Check if date is close to deadline, then create notification
+    const checkDeadline = (deadlineDate) => {
+        // Get today's date
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // Get date from calendar
+        const calendarDate = new Date(deadlineDate);
+        calendarDate.setHours(0, 0, 0, 0);
+
+        // Get date 3 days before deadline
+        const threeDaysBeforeDeadline = new Date(calendarDate);
+        threeDaysBeforeDeadline.setDate(threeDaysBeforeDeadline.getDate() - 3);
+        threeDaysBeforeDeadline.setHours(0, 0, 0, 0);
+
+        // Get date 1 day before deadline
+        const oneDayBeforeDeadline = new Date(calendarDate);
+        oneDayBeforeDeadline.setDate(oneDayBeforeDeadline.getDate() - 1);
+        oneDayBeforeDeadline.setHours(0, 0, 0, 0);
+
+        // Create notification if today's date is 3 days before deadline
+        if (today >= threeDaysBeforeDeadline && today < oneDayBeforeDeadline) {
+            createNotification("warning", "3 days before deadline!");
+        }
+
+        // Create notification if today's date is 1 day before deadline
+        if (today >= oneDayBeforeDeadline && today < calendarDate) {
+            createNotification("warning", "1 day before deadline!");
+        }
+
+        // Create notification if today's date is deadline
+        if (today >= calendarDate) {
+            createNotification("info", "Deadline!");
+        }
+    }
+
 
     const calendarClickHandler = (e) => {
         // Get date clicked on calendar
@@ -39,7 +101,24 @@ const CalendarComponent = () => {
         else {
             setSelectedTask(null);
         }
+    }
 
+    // Create Notification
+    const createNotification = (type, message) => {
+        switch (type) {
+            case "info":
+                toast.info(message);
+                break;
+            case "success":
+                toast.success(message);
+                break;
+            case "warning":
+                toast.warn(message);
+                break;
+            case "error":
+                toast.error(message);
+                break;
+        }
     }
 
     return (
@@ -52,9 +131,9 @@ const CalendarComponent = () => {
                     <label>Task Name</label>
                     <input type="text" name="taskName" placeholder='Job Interview' />
                     <label>Task Description</label>
-                    <input type="text" name="taskDescription" placeholder='Study'/>
+                    <input type="text" name="taskDescription" placeholder='Study' />
                     <label>Note</label>
-                    <input type="text" name="note" placeholder='Need to pass'/>
+                    <input type="text" name="note" placeholder='Need to pass' />
                     <label>Deadline</label>
                     <input type="date" name="deadline" />
                     <label>Remind Me</label>
@@ -62,7 +141,6 @@ const CalendarComponent = () => {
                     <input type="submit" value="Submit" />
                 </form>
             </div>
-
             <div>
             {
                 // If selectedTask is not null, loop and render Task component
@@ -75,6 +153,7 @@ const CalendarComponent = () => {
                 
             }
             </div>
+            <ToastContainer />
         </>
     )
 }
